567bfbbcbda15d15294d4f912ba2f95afc0c0f10
RamRajasagi <35080210+RamRajasagi@users.noreply.github.com>
2023-12-10 11:44:30 -0500

    with new csproj

---
+++ ch1/CS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ch1
+{
+    class ch1_ans
+    {
+        static void Main(string[] args) // Code runs first from this method
+        {
+            q1();
+        }
+
+        public static void q1()//unique alphas without any data structure
+        {
+            var str1 = "tes";
+            int slen =
+            Console.WriteLine("test");
+        }
+    }
+
+
+}
