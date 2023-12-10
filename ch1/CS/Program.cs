01c1e79d864adb9ad0f4ef1a667dd65fc798809e
RamRajasagi <35080210+RamRajasagi@users.noreply.github.com>
2023-12-10 15:14:17 -0500

    cs ch1_1

--- ch1/CS/Program.cs
+++ ch1/CS/Program.cs
@@ -11,9 +11,24 @@ namespace ch1
 
         public static void q1()//unique alphas without any data structure
         {
-            var str1 = "tes";
-            int slen =
-            Console.WriteLine("test");
+            var str1 = "tessqwrt";
+            int slen = str1.Length;
+
+            for (int i = 0; i < slen - 1; i++)
+            {
+                //Console.WriteLine($"i value is:{str1[i]}");
+                for (int j = i + 1; j < slen; j++)
+                {
+                    //Console.WriteLine($"j value is:{str1[j]}");
+                    if (str1[i] == str1[j])
+                    {
+                        Console.WriteLine($"Dup found");
+                        return;
+                    }
+
+                }
+            }
+
         }
     }
 
